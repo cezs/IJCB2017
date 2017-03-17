fe3bc620b5c3b02104010b87e67f181f97dc4a90
cezs <diagram.chasing@gmail.com>
2017-03-17 14:24:58 +0000

    Remove duplicated line

--- scripts/cs_regenerate.sh
+++ scripts/cs_regenerate.sh
@@ -1,6 +1,5 @@
 rm /home/cs/_/IJCB2017/ijcb2017*
 rm /home/cs/_/IJCB2017/training/*.txt
 rm /home/cs/_/IJCB2017/training_resized/*.txt
-rm /home/cs/_/IJCB2017/training_resized/*.txt
 python /home/cs/_/IJCB2017/scripts/cs_resize.py
 python /home/cs/_/IJCB2017/scripts/cs_convert_to_darknet.py
