97fac0f075b01e61172fbed1f09e2a72d73dc7dc
cezs <diagram.chasing@gmail.com>
2017-03-20 22:50:29 +0000

    Split classes list into labels and names
    Labels list stores numbers corresponding to classes' names stored in names list.

--- scripts/cs_convert_to_darknet.py
+++ scripts/cs_convert_to_darknet.py
@@ -6,30 +6,30 @@ use_original_classes = False
 
 if not use_resized_dataset:
     input_training_listing = '/media/win/_/IJCB2017/protocol/training_updated.csv'
-    output_training_listing ='/media/win/_/IJCB2017/ijcb2017_training_updated.txt'
+    output_training_listing ='/media/win/_/IJCB2017/ijcb2017_updated.list'
     training_images = '/media/win/_/IJCB2017/training'
 
     input_validation_listing = '/media/win/_/IJCB2017/protocol/validation.csv'
-    output_validation_listing ='/media/win/_/IJCB2017/ijcb2017_validation.txt'
+    output_validation_listing ='/media/win/_/IJCB2017/ijcb2017.valid'
     validation_images = '/media/win/_/IJCB2017/validation'
 
-    img_height = 5184.0
-    img_width = 3456.0
+    img_height = 3456.0
+    img_width = 5184.0
 else:
     input_training_listing = '/media/win/_/IJCB2017/protocol/training_resized.csv'
-    output_training_listing ='/media/win/_/IJCB2017/ijcb2017_training_resized.txt'
+    output_training_listing ='/media/win/_/IJCB2017/ijcb2017_resized.list'
     training_images = '/media/win/_/IJCB2017/training_resized'
 
     input_validation_listing = '/media/win/_/IJCB2017/protocol/validation.csv'
-    output_validation_listing ='/media/win/_/IJCB2017/ijcb2017_validation.txt'
+    output_validation_listing ='/media/win/_/IJCB2017/ijcb2017.valid'
     validation_images = '/media/win/_/IJCB2017/validation'
 
-    img_height = 576.0
-    img_width = 384.0
+    img_height = 384.0
+    img_width = 576.0
 
-#######################
-# train files listing #
-#######################
+######################################################
+# covert to darknet format and store in python lists #
+######################################################
 
 train_filenames = []
 subject_id= []
@@ -39,38 +39,52 @@ width = []
 height = []
 
 # append each image filename for training to list 
-with open(input_training_listing, 'rb') as in_train_listing:
+with open(input_training_listing, 'r') as in_train_listing:
     reader = csv.reader(in_train_listing)
     next(reader)
     for row in reader:
         train_filenames.append(row[1])
         if (use_original_classes):
             # use original class
-            subject_id.append(row[2])
+            # TODO: map original subject_id to label
+            # instead of using int() + 1 hack
+            subject_id.append(int(row[2]) + 1)
         else:
-            # use binary classification
-            if (row[2] >= 0):
-                subject_id.append(1)
-            else:
-                subject_id.append(0)
+            # just detect
+            subject_id.append(1)
         # recalculate starting position of bounding box to the center
         # of it instead of the supplied upper left corner and make
         # position as well as dimensions of this bounding box relative
         # to image dimensions
-        x.append( (float(row[3]) + float(row[5]) / 2.0) / img_width)
-        y.append( (float(row[4]) + float(row[6]) / 2.0) / img_height)
+        x.append((float(row[3]) + float(row[5]) / 2.0) / img_width)
+        y.append((float(row[4]) + float(row[6]) / 2.0) / img_height)
         width.append(float(row[5]) / img_width)
         height.append(float(row[6]) / img_height)
 
 
+classes = []
+labels = range(len(classes))
+# append each image filename for training to list 
+with open(input_training_listing, 'r') as in_train_listing:
+    reader = csv.reader(in_train_listing)
+    next(reader)
+    for row in reader:
+        classes.append(int(row[2]) + 1)
+print(len(set(classes)))
+
+#################
+# training list #
+#################
+
 # print converted listing of images for training 
-with open(output_training_listing, 'wb') as out_train_listing:
+with open(output_training_listing, 'w') as out_train_listing:
+    # note the use of set to from list from only unique filenames
     for f in set(train_filenames):
         out_train_listing.write("{}/{}\n".format(training_images, f))
 
-#############################
-# individual training files #
-#############################
+###################################
+# individual training annotations #
+###################################
 
 # make list of filenames for files which are going to store individual
 # id, x, y, width and height
@@ -80,32 +94,48 @@ for f in train_filenames:
     individual_filenames.append(f.rsplit(".", 1)[0] + '.txt')
     
 for i in range(len(x)):
-    with open("{}/{}".format(training_images, individual_filenames[i]), 'w') as newfile:
+    with open("{}/{}".format(training_images, individual_filenames[i]), 'a') as newfile:
         newfile.write("{} {} {} {} {}\n".format(subject_id[i], \
                                                 x[i], \
                                                 y[i], \
                                                 width[i], \
                                                 height[i]))
 
-##########################
-# all classes names file #
-##########################
+###############
+# labels file #
+###############
 
-classes_file = '/media/win/_/IJCB2017/ijcb2017.names'
+labels_file = '/media/win/_/IJCB2017/ijcb2017.labels'
 
-with open(classes_file, 'wb') as classes:
+with open(labels_file, 'wb') as labels:
+        if (use_original_classes):
+            # use original classification
+            for i in range(len(set(subject_id))):
+                labels.write("{}\n".format(i))
+        else:
+            # use binary classification
+            labels.write("{}\n".format(0))
+            labels.write("{}\n".format(1))
+
+##############
+# names file #
+##############
+
+names_file = '/media/win/_/IJCB2017/ijcb2017.names'
+
+with open(names_file, 'wb') as names:
         if (use_original_classes):
             # use original classification
             for i in set(subject_id):
-                classes.write("{}\n".format(i))
+                names.write("{}\n".format(i))
         else:
             # use binary classification
-            classes.write("{}\n".format(0))
-            classes.write("{}\n".format(1))
+            names.write("{}\n".format("nothing"))
+            names.write("{}\n".format("face"))
 
-############################
-# validation files listing #
-############################
+##########################
+# validation images list #
+##########################
 
 v_filenames = []
 v_subject_id= []
@@ -115,7 +145,7 @@ v_width = []
 v_height = []
 
 # append each image filename for validation to list 
-with open(input_validation_listing, 'rb') as in_validation_listing:
+with open(input_validation_listing, 'r') as in_validation_listing:
     reader = csv.reader(in_validation_listing)
     next(reader)
     for row in reader:
@@ -124,11 +154,8 @@ with open(input_validation_listing, 'rb') as in_validation_listing:
             # use original class
             v_subject_id.append(row[2])
         else:
-            # use binary classification
-            if (row[2] >= 0):
-                v_subject_id.append(1)
-            else:
-                v_subject_id.append(0)
+            # just detect
+            v_subject_id.append(1)
         # recalculate starting position of bounding box to the center
         # of it instead of the supplied upper left corner and make
         # position as well as dimensions of this bounding box relative
@@ -144,9 +171,9 @@ with open(output_validation_listing, 'wb') as out_validation_listing:
     for f in set(v_filenames):
         out_validation_listing.write("{}/{}\n".format(validation_images, f))
 
-###############################
-# individual validation files #
-###############################
+#####################################
+# individual validation annotations #
+#####################################
 
 # make list of filenames for files which are going to store individual
 # id, x, y, width and height
@@ -156,7 +183,7 @@ for f in v_filenames:
     v_individual_filenames.append(f.rsplit(".", 1)[0] + '.txt')
     
 for i in range(len(v_x)):
-    with open("{}/{}".format(validation_images, v_individual_filenames[i]), 'w') as newfile:
+    with open("{}/{}".format(validation_images, v_individual_filenames[i]), 'a') as newfile:
         newfile.write("{} {} {} {} {}\n".format(v_subject_id[i], \
                                                 v_x[i], \
                                                 v_y[i], \
@@ -177,20 +204,19 @@ with open(config_file, 'wb') as cfg_file:
         # use binary classification
         cfg_file.write("{} = {}\n".format("classess", 2))
     cfg_file.write("{} = {}\n".format("train", output_training_listing))
-    cfg_file.write("{} = {}\n".format("valid", output_validation_listing))
-    # cfg_file.write("{} = {}\n".format("valid", output_test_listing)) 
-    cfg_file.write("{} = {}\n".format("names", classes_file))
+    cfg_file.write("{} = {}\n".format("valid", output_training_listing))
+    cfg_file.write("{} = {}\n".format("labels", labels_file))
+    cfg_file.write("{} = {}\n".format("names", names_file))
     cfg_file.write("{} = {}\n".format("backup","backup"))
 
+# ######################
+# # test files listing #
+# ######################
 
 # NOTE: Test dataset has yet to be released on 04/17/2017
 
 # input_test_listing = '/media/win/_/IJCB2017/protocol/test.csv'
 # output_test_listing = '/media/win/_/IJCB2017/ijcb2017_test.txt'
-
-# ######################
-# # test files listing #
-# ######################
 
 # # store filenames of images for testing in list
 # test_filenames = []
