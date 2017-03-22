f94cfc8103d7603c7f0035a64cb6266abd36c823
cezs <diagram.chasing@gmail.com>
2017-03-22 17:39:14 +0000

    Threefold contribution
    1. Append only if subject_id is not equal to -1
    2. Add Paths and Config classes.
    3. Add getArgs() and run(config, paths) functions.

--- scripts/cs_convert_ijcb2017_to_darknet.py
+++ scripts/cs_convert_ijcb2017_to_darknet.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import os
-import csv
+import sys
 import argparse
+import csv
 
 class bcolors:
     HEADER = '\033[95m'
@@ -18,7 +19,7 @@ class bcolors:
 def script_info(msg):
     print bcolors.HEADER + "[INFO] " + str(msg) + bcolors.ENDC
 
-def ijcb2017_to_darknet(ijcb2017_annotations, use_original_classes):
+def ijcb2017_to_darknet(ijcb2017_annotations, use_original_classes, img_width, img_height):
     """
     Covert to darknet format and store in python lists.
     Returns bboxes with classes and corresponding images filenames.
@@ -39,7 +40,10 @@ def ijcb2017_to_darknet(ijcb2017_annotations, use_original_classes):
             if (use_original_classes):
                 # use original class
                 # TODO: map original subject_id to label
-                subject_id.append(int(row[2]))
+                if not int(row[2]) == -1:
+                    subject_id.append(int(row[2]))
+                else:
+                    continue
             else:
                 # just detect
                 subject_id.append(1)
@@ -62,7 +66,7 @@ def create_darknet_list(output_list, prepended_directory, input_list):
         for f in set(input_list):
             out_list.write("{}/{}\n".format(prepended_directory, f))
 
-def create_darknet_annotations(use_original_classes, input_list, output_directory, \
+def create_darknet_annotations(input_list, output_directory, \
                                subject_id, x, y, width, height):
     """
     Create individual training annotations i.e., make list of filenames for 
@@ -73,35 +77,21 @@ def create_darknet_annotations(use_original_classes, input_list, output_director
     for f in input_list:
         individual_filenames.append(f.rsplit(".", 1)[0] + '.txt')
 
-    if (use_original_classes):
-        for i in range(len(x)):
-            if not subject_id[i] == -1:
-                with open("{}/{}".format(output_directory, \
-                                         individual_filenames[i]), 'a') as newfile:
-                    newfile.write("{} {} {} {} {}\n".format(subject_id[i], \
-                                                            x[i], \
-                                                            y[i], \
-                                                            width[i], \
-                                                            height[i]))
-    else:
-        for i in range(len(x)):
-            with open("{}/{}".format(output_directory, \
-                                     individual_filenames[i]), 'a') as newfile:
-                newfile.write("{} {} {} {} {}\n".format(subject_id[i], \
-                                                        x[i], \
-                                                        y[i], \
-                                                        width[i], \
-                                                        height[i]))
-        
+    for i in range(len(x)):
+        with open("{}/{}".format(output_directory, \
+                                 individual_filenames[i]), 'a') as newfile:
+            newfile.write("{} {} {} {} {}\n".format(subject_id[i], \
+                                                    x[i], \
+                                                    y[i], \
+                                                    width[i], \
+                                                    height[i]))
 
 def create_labels_file(labels_file, use_original_classes, subject_id):
     """Create labels file."""
     with open(labels_file, 'wb') as labels:
         if (use_original_classes):
             # use original classification
-            original_classification = sorted(set(subject_id))
-            original_classification.remove(-1)
-            for i in range(len(original_classification)):
+            for i in range(len(sorted(set(subject_id)))):
                 labels.write("{}\n".format(i))
         else:
             # use binary classification
@@ -113,21 +103,25 @@ def create_names_file(names_file, use_original_classes, subject_id):
     with open(names_file, 'wb') as names:
             if (use_original_classes):
                 # use original classification
-                original_classification = sorted(set(subject_id))
-                original_classification.remove(-1)
-                for i in original_classification:
+                for i in sorted(set(subject_id)):
                     names.write("{}\n".format(i))
             else:
                 # use binary classification
                 names.write("{}\n".format("nothing"))
                 names.write("{}\n".format("face"))
 
-def create_darknet_data_configuration_file(config_file):
+def create_darknet_data_configuration_file(data_config_file, \
+                                           use_original_classes, \
+                                           subject_id, \
+                                           output_training_listing, \
+                                           labels_file, \
+                                           names_file, \
+                                           backup_dir):
     """Create configuration file."""
-    with open(config_file, 'wb') as cfg_file:
+    with open(data_config_file, 'wb') as cfg_file:
         if (use_original_classes):
             # use original classification
-            cfg_file.write("{} = {}\n".format("classess",len(set(subject_id))))
+            cfg_file.write("{} = {}\n".format("classess", len(set(subject_id))))
         else:
             # use binary classification
             cfg_file.write("{} = {}\n".format("classess", 2))
@@ -135,11 +129,83 @@ def create_darknet_data_configuration_file(config_file):
         cfg_file.write("{} = {}\n".format("valid", output_training_listing))
         cfg_file.write("{} = {}\n".format("labels", labels_file))
         cfg_file.write("{} = {}\n".format("names", names_file))
-        cfg_file.write("{} = {}\n".format("backup","backup"))
+        cfg_file.write("{} = {}\n".format("backup", backup_dir))
 
-if __name__ == "__main__":
+class Paths(dict):
+    def __init__(self, **kwargs):
+        super(Paths, self).__init__(**kwargs)
+        self.__dict__ = self
 
-    parser = argparse.ArgumentParser(description="Unconstrained Face Detection and Open Set Recognition Challenge in Darknet")
+class Config(dict):
+    def __init__(self, **kwargs):
+        super(Config, self).__init__(**kwargs)
+        self.__dict__ = self
+
+def run(cfg, paths):
+    "Run program"
+    if cfg.use_resized_dataset:
+        script_info("Using resized dataset")
+    else:
+        script_info("Using original dataset")
+
+    if cfg.use_original_classes:
+        script_info("Using original classification")
+    else:
+        script_info("Using binary classification")
+
+    train_filenames, subject_id, x, y, width, height = \
+        ijcb2017_to_darknet(paths.input_training_listing, cfg.use_original_classes, paths.img_width, paths.img_height)
+        
+    if cfg.do_create_darknet_list_for_training:
+        create_darknet_list(paths.output_training_listing, \
+                             paths.training_images, \
+                             train_filenames)
+        script_info("Created darknet list for training")
+
+    if cfg.do_create_darknet_annotations_for_training:
+        create_darknet_annotations(train_filenames, \
+                                   paths.training_images, subject_id, \
+                                   x, y, width, height)
+        script_info("Created darknet annotations for training")
+
+    v_filenames, v_subject_id, v_x, v_y, v_width, v_height = \
+        ijcb2017_to_darknet(paths.input_validation_listing, cfg.use_original_classes, paths.img_width, paths.img_height)
+
+    if cfg.do_create_darknet_list_for_validation:
+        create_darknet_list(paths.output_validation_listing, \
+                             paths.validation_images, \
+                             v_filenames)
+        script_info("Created darknet list for validation")
+
+    if cfg.do_create_darknet_annotations_for_validation:
+        create_darknet_annotations(v_filenames, \
+                                   paths.validation_images, v_subject_id, \
+                                   v_x, v_y, v_width, v_height)
+        script_info("Created darknet annotations for validation")
+
+    if cfg.do_create_labels_file:
+        create_labels_file(paths.labels_file, cfg.use_original_classes, subject_id)
+        script_info("Created labels file")
+
+    if cfg.do_create_names_file:
+        create_names_file(paths.names_file, cfg.use_original_classes, subject_id)
+        script_info("Created names file")
+
+    if cfg.do_create_configuration_file:
+        create_darknet_data_configuration_file(paths.data_config_file, \
+                                               cfg.use_original_classes, \
+                                               subject_id, \
+                                               paths.output_training_listing, \
+                                               paths.labels_file, \
+                                               paths.names_file, \
+                                               paths.backup_dir)
+        script_info("Created configuration file")
+
+
+def getArgs():
+    parser = argparse.ArgumentParser(
+        description="Unconstrained Face Detection and Open Set Recognition" \
+        "Challenge in Darknet")
     parser.add_argument('-a', '--resized_dataset', dest='a', action='store_true',
                         help='Use resized dataset')
     parser.add_argument('-b', '--original_classes', dest='b', action='store_true',
@@ -162,91 +228,64 @@ if __name__ == "__main__":
 
     print(bcolors.BOLD + "Entered main function" + bcolors.ENDC)
 
-    use_resized_dataset = args.a
-    use_original_classes = args.b
-    do_create_darknet_list_for_training = args.c
-    do_create_darknet_annotations_for_training = args.d
-    do_create_darknet_list_for_validation = args.e
-    do_create_darknet_annotations_for_validation = args.f
-    do_create_labels_file = args.g
-    do_create_names_file = args.i
-    do_create_configuration_file = args.j
+    # if args empty
+    if not len(sys.argv) > 1:
+        # cfg = Config(True,False,True,True,True,True,True,True,True)
+        cfg = Config(use_resized_dataset = True,\
+                     use_original_classes = False,\
+                     do_create_darknet_list_for_training = True,\
+                     do_create_darknet_annotations_for_training = True,\
+                     do_create_darknet_list_for_validation = True,\
+                     do_create_darknet_annotations_for_validation = True,\
+                     do_create_labels_file = True,\
+                     do_create_names_file = True,\
+                     do_create_configuration_file = True)
+    else:
+        cfg = Config(use_resized_dataset = args.a,\
+                     use_original_classes = args.b,\
+                     do_create_darknet_list_for_training = args.c,\
+                     do_create_darknet_annotations_for_training = args.d,\
+                     do_create_darknet_list_for_validation = args.e,\
+                     do_create_darknet_annotations_for_validation = args.f,\
+                     do_create_labels_file = args.g,\
+                     do_create_names_file = args.i,\
+                     do_create_configuration_file = args.j)
     
-    config_file = '/media/win/_/IJCB2017/cfg/ijcb2017.data'
-    labels_file = '/media/win/_/IJCB2017/data/ijcb2017.labels'
-    names_file = '/media/win/_/IJCB2017/data/ijcb2017.names'
+    paths = Paths(data_config_file = '/media/win/_/IJCB2017/cfg/ijcb2017.data', \
+                  labels_file = '/media/win/_/IJCB2017/data/ijcb2017.labels', \
+                  names_file = '/media/win/_/IJCB2017/data/ijcb2017.names', \
+                  backup_dir =  '/media/win/_/IJCB2017/backup', \
 
-    if not use_resized_dataset:
-        input_training_listing = '/media/win/_/IJCB2017/protocol/training_updated.csv'
-        output_training_listing ='/media/win/_/IJCB2017/ijcb2017_updated.train.list'
-        training_images = '/media/win/_/IJCB2017/training'
+                  input_training_listing = '', \
+                  output_training_listing ='/media/win/_/IJCB2017/ijcb2017.train.list',\
+                  training_images = '', \
 
-        input_validation_listing = '/media/win/_/IJCB2017/protocol/validation.csv'
-        output_validation_listing ='/media/win/_/IJCB2017/ijcb2017.valid.list'
-        validation_images = '/media/win/_/IJCB2017/validation'
+                  input_validation_listing = '/media/win/_/IJCB2017/protocol/validation.csv', \
+                  output_validation_listing ='/media/win/_/IJCB2017/ijcb2017.valid.list', \
+                  validation_images = '/media/win/_/IJCB2017/validation', \
 
-        img_height = 3456.0
-        img_width = 5184.0
+                  img_height = 0, \
+                  img_width = 0)
+
+    if not cfg.use_resized_dataset:
+        paths.input_training_listing = '/media/win/_/IJCB2017/protocol/training_updated.csv'
+        paths.training_images = '/media/win/_/IJCB2017/training'
+
+        # TODO: Use library to get dimensions and remove from here
+        paths.img_height = 3456.0
+        paths.img_width = 5184.0
     else:
-        input_training_listing = '/media/win/_/IJCB2017/protocol/training_resized.csv'
-        output_training_listing ='/media/win/_/IJCB2017/ijcb2017_resized.train.list'
-        training_images = '/media/win/_/IJCB2017/training_resized'
+        paths.input_training_listing = '/media/win/_/IJCB2017/protocol/training_resized.csv'
+        paths.training_images = '/media/win/_/IJCB2017/training_resized'
 
-        input_validation_listing = '/media/win/_/IJCB2017/protocol/validation.csv'
-        output_validation_listing ='/media/win/_/IJCB2017/ijcb2017.valid.list'
-        validation_images = '/media/win/_/IJCB2017/validation'
+        # TODO: Use library to get dimensions and remove from here
+        paths.img_height = 384.0
+        paths.img_width = 576.0
 
-        img_height = 384.0
-        img_width = 576.0
+    return cfg, paths
 
-    if use_resized_dataset:
-        script_info("Using resized dataset")
-    else:
-        script_info("Using original dataset")
+if __name__ == "__main__":
+    
+    cfg, paths = getArgs()
 
-    if use_original_classes:
-        script_info("Using original classification")
-    else:
-        script_info("Using binary classification")
-
-    train_filenames, subject_id, x, y, width, height = \
-        ijcb2017_to_darknet(input_training_listing, use_original_classes)
-        
-    if do_create_darknet_list_for_training:
-        create_darknet_list(output_training_listing, \
-                             training_images, \
-                             train_filenames)
-        script_info("Created darknet list for training")
-
-    if do_create_darknet_annotations_for_training:
-        create_darknet_annotations(use_original_classes, train_filenames, \
-                                   training_images, subject_id, \
-                                   x, y, width, height)
-        script_info("Created darknet annotations for training")
-
-    v_filenames, v_subject_id, v_x, v_y, v_width, v_height = \
-        ijcb2017_to_darknet(input_validation_listing, use_original_classes)
-
-    if do_create_darknet_list_for_validation:
-        create_darknet_list(output_validation_listing, \
-                             validation_images, \
-                             v_filenames)
-        script_info("Created darknet list for validation")
-
-    if do_create_darknet_annotations_for_validation:
-        create_darknet_annotations(use_original_classes, v_filenames, \
-                                   validation_images, v_subject_id, \
-                                   v_x, v_y, v_width, v_height)
-        script_info("Created darknet annotations for validation")
-
-    if do_create_labels_file:
-        create_labels_file(labels_file, use_original_classes, subject_id)
-        script_info("Created labels file")
-
-    if do_create_names_file:
-        create_names_file(names_file, use_original_classes, subject_id)
-        script_info("Created names file")
-
-    if do_create_configuration_file:
-        create_darknet_data_configuration_file(config_file)
-        script_info("Created configuration file")
+    run(cfg, paths)
