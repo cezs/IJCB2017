399adf3e211e61257d02be0a9c1a748c58b66f16
cezs <diagram.chasing@gmail.com>
2017-03-22 14:18:59 +0000

    Print images with detected bounding boxes to individual files

--- scripts/cs_darknet_run_detection.sh
+++ scripts/cs_darknet_run_detection.sh
@@ -1,14 +1,23 @@
 current_dir=$(pwd)
 
 darknet="/home/cs/Documents/packages/darknet_fork"
-configuration="/media/win/_/IJCB2017/cfg/my-yolo-voc-2-416.cfg"
-weights="/home/cs/Documents/packages/darknet_fork/backup/my-yolo-voc-2-416_11000.weights"
+data_cfg="/media/win/_/IJCB2017/cfg/ijcb2017-yolo9000-uno.data"
+network_cfg="/media/win/_/IJCB2017/cfg/ijcb2017-yolo9000-uno.cfg"
+weights="/media/win/_/IJCB2017/weights/ijcb2017-yolo9000-uno_3000.weights"
 images="/media/win/_/IJCB2017/training_resized"
+bboxes="/media/win/_/IJCB2017/bboxes"
+
+thresh=0.001
 
 cd $darknet
 
+mkdir $bboxes
+
 for filename in $images/*.jpg; do
-	$darknet/darknet detect $configuration $weights $filename -thresh 0.01
+    $darknet/darknet detector test $data_cfg $network_cfg $weights $filename -thresh $thresh;
+    image_with_detections=$(echo $(basename "$filename" ".jpg")_detections.png
+    mv "predictions.png" $bboxes/$image_with_detections)
+    echo "[  OK  ] Created $filename";
 done
 
 cd $curent_dir
