783de1b87531d268c82e57ef333eeaf87fe72821
cezs <diagram.chasing@gmail.com>
2017-03-22 14:08:29 +0000

    Update regeneration script

--- scripts/cs_regenerate.sh
+++ scripts/cs_regenerate.sh
@@ -3,4 +3,5 @@ rm /home/cs/_/IJCB2017/training/*.txt
 rm /home/cs/_/IJCB2017/training_resized/*.txt
 rm /home/cs/_/IJCB2017/validation/*.txt
 python /home/cs/_/IJCB2017/scripts/cs_resize.py
-python /home/cs/_/IJCB2017/scripts/cs_convert_to_darknet.py
+# python /home/cs/_/IJCB2017/scripts/cs_convert_ijcb2017_to_darknet.py -abcdefgij
+python /home/cs/_/IJCB2017/scripts/cs_convert_ijcb2017_to_darknet.py -acdefgij
