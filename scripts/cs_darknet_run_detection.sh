c97c181de091e944034b7ecfa8df3e419156a80a
cezs <diagram.chasing@gmail.com>
2017-03-23 05:09:46 +0000

    Change paths

--- scripts/cs_darknet_run_detection.sh
+++ scripts/cs_darknet_run_detection.sh
@@ -1,13 +1,20 @@
 current_dir=$(pwd)
 
+# darknet="/home/cs/Documents/packages/darknet_fork"
+# data_cfg="/media/win/_/IJCB2017/cfg/ijcb2017-yolo9000-uno.data"
+# network_cfg="/media/win/_/IJCB2017/cfg/ijcb2017-yolo9000-uno.cfg"
+# weights="/media/win/_/IJCB2017/weights/ijcb2017-yolo9000-uno_3000.weights"
+# images="/media/win/_/IJCB2017/training_resized"
+# bboxes="/media/win/_/IJCB2017/bboxes"
+
 darknet="/home/cs/Documents/packages/darknet_fork"
-data_cfg="/media/win/_/IJCB2017/cfg/ijcb2017-yolo9000-uno.data"
-network_cfg="/media/win/_/IJCB2017/cfg/ijcb2017-yolo9000-uno.cfg"
-weights="/media/win/_/IJCB2017/weights/ijcb2017-yolo9000-uno_3000.weights"
+data_cfg="/media/win/_/IJCB2017/cfg/ijcb2017-yolo-voc.data"
+network_cfg="/media/win/_/IJCB2017/cfg/ijcb2017-yolo-voc-train.cfg"
+weights="/media/win/_/IJCB2017/weights/ijcb2017-yolo-voc-train_15000.weights"
 images="/media/win/_/IJCB2017/training_resized"
 bboxes="/media/win/_/IJCB2017/bboxes"
 
-thresh=0.001
+thresh=0.5
 
 cd $darknet
 
@@ -17,7 +24,7 @@ for filename in $images/*.jpg; do
     $darknet/darknet detector test $data_cfg $network_cfg $weights $filename -thresh $thresh;
     image_with_detections=$(echo $(basename "$filename" ".jpg")_detections.png
     mv "predictions.png" $bboxes/$image_with_detections)
-    echo "[  OK  ] Created $filename";
+    echo "[  OK  ] Created $image_with_detections";
 done
 
 cd $curent_dir
