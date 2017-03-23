3e4a2a278ed21398be1157dbd67c063bc50dd9cb
cezs <diagram.chasing@gmail.com>
2017-03-23 05:18:03 +0000

    Various additions
    1. Add offset parameter
    2. Try list comprehensions
    3. Graph collected data

--- scripts/cs_darknet_valid_plot.py
+++ scripts/cs_darknet_valid_plot.py
@@ -1,13 +1,17 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# offset = 10000
+offset = 0
+
 # given original list
-training_list_path='/home/cs/remote/titanx/media/win/_/IJCB2017/protocol/training_resized.csv'[0:]
+training_list_path='/home/cs/remote/titanx/media/win/_/IJCB2017/protocol/training_resized.csv'[22:]
 
 # and prediction list
-prediction_list_path='/home/cs/remote/titanx/media/win/_/IJCB2017/valid/ijcb2017-yolo-voc-0.5/comp4_det_test_face.txt'[0:]
+prediction_list_path='/home/cs/remote/titanx/media/win/_/IJCB2017/valid/ijcb2017-yolo-voc-0.5/comp4_det_test_face.txt'[22:]
 
 def read_original_file(list_path):
+    # v0
     # get images filenames and classifcations list
     ijcb2017_original_list = []
     with open(list_path, 'rb') as training_list:
@@ -17,6 +21,14 @@ def read_original_file(list_path):
             label = str(fields[2])
             ijcb2017_original_list.append([image_name, label])
 
+    # # v1
+    # ijcb2017_original_list = []
+    # with open(list_path, 'rb') as training_list:
+    #     ijcb2017_original_list = [[str(line.strip().split(',')[1].replace(".jpg","")), \
+    #                                str(line.strip().split(',')[2])] \
+    #                               for line in training_list]
+
+    # v0
     # get just images names list
     occurence_training = []
     for row in ijcb2017_original_list:
@@ -25,14 +37,19 @@ def read_original_file(list_path):
 
     # count occurencies of each image in list
     image_vs_occurence_original = []
-    unique_filenames_from_original_list = set([row[0] for row in ijcb2017_original_list])
     i = 0
-    for image_name in unique_filenames_from_original_list:
-        if (i < 10000):
+    # for each unique filename from original list
+    for image_name in set(occurence_training):
+        if (i < offset):
             i += 1
         else:
             image_name_count = occurence_training.count(image_name)
             image_vs_occurence_original.append([image_name, image_name_count])
+
+    # # v1
+    # occurence_training = [row[0] for row in ijcb2017_original_list] 
+    # image_vs_occurence_original = [[image_name, occurence_training.count(image_name)] \
+    #                                for image_name in set(occurence_training)]
 
     return image_vs_occurence_original
 
@@ -61,7 +78,7 @@ def image_vs_occurence(prediction_list, threshold):
             filenames_list.append(image_filename)
     # count all detections for each image
     for image_name in unique_names:
-        if (i < 10000):
+        if (i < offset):
             i += 1
         else:
             image_name_count = filenames_list.count(image_name)
@@ -75,11 +92,20 @@ predictions_list = read_predictions_file(prediction_list_path)
 
 tp_sum = sum(truepositives)
 
-for i in np.linspace(0, 1, 100):
+tp = []
+fp = []
+
+for i in np.linspace(0, 1, 4):
     image_vs_occurence_predicted = image_vs_occurence(predictions_list, i)
     truepositives_and_falsepositives = [elem[1] for elem in image_vs_occurence_predicted]
     fp_sum = sum(truepositives_and_falsepositives) - tp_sum
+    tp.append(tp_sum)
+    fp.append(fp_sum)
     print('thresh: {} tp: {} fp: {} tp/(tp+fp): {}'.format(i, \
                                                            tp_sum, \
                                                            fp_sum, \
                                                            tp_sum / float((tp_sum+fp_sum))))
+
+
+plt.plot(tp,'.-')
+plt.show()
