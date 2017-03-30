5cd6ef10635a677e6e388e5b5cd14f41d9b201a3
cezs <diagram.chasing@gmail.com>
2017-03-30 11:58:06 +0100

    Add comments

--- scripts/cs_regenerate.sh
+++ scripts/cs_regenerate.sh
@@ -1,7 +1,14 @@
+# remove generated files
 rm /home/cs/_/IJCB2017/ijcb2017*
 rm /home/cs/_/IJCB2017/training/*.txt
 rm /home/cs/_/IJCB2017/training_resized/*.txt
 rm /home/cs/_/IJCB2017/validation/*.txt
+
+# update csv file
 python /home/cs/_/IJCB2017/scripts/cs_resize.py
-# python /home/cs/_/IJCB2017/scripts/cs_convert_ijcb2017_to_darknet.py -abcdefgij
-python /home/cs/_/IJCB2017/scripts/cs_convert_ijcb2017_to_darknet.py -acdefgij
+
+# generate files for multi-class classifications
+python /home/cs/_/IJCB2017/scripts/cs_convert_ijcb2017_to_darknet.py -abcdefgij
+
+# # generate files for binary classification
+# python /home/cs/_/IJCB2017/scripts/cs_convert_ijcb2017_to_darknet.py -acdefgij
